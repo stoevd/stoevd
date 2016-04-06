f9790a033bd4d43b19c0ba52810075d698bda48a
stoevd <stoevd@uni-sofia.bg>
2016-04-06 09:39:45 +0300

    DimitarDimitrov

---
+++ Fahrenheit-Celsius/Fahrenheit-Celsius/Fahrenheit-Celsius.cpp
@@ -0,0 +1,78 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+
+	//printf("Hello World\n"); // 1-1
+	
+
+
+	//float fahr; // 1-4
+
+	//printf("FAHRENHEIT\tCELSIUS\n");
+
+	//for (fahr = 0; fahr <= 300; fahr = fahr + 20)
+	//{
+	//	printf("%.2f\t\t%.2f\n", fahr, ((fahr - 32) * 5 / 9));
+	//}
+
+
+
+
+	/*int celsius; // 1-4
+
+	for (celsius = 0; celsius <= 300; celsius = celsius + 20)
+	{
+		printf("%d\t%d\n", celsius, (celsius * 9 / 5 + 32));
+	}*/
+
+
+
+
+	/*int c; // 1-6
+	while ((c = getchar()) != eof) {
+		printf("%d ", c != eof);
+		putchar(c);
+	}
+	printf("\n%d\n", c != eof);
+*/
+	
+
+
+
+	//int c; // 1-12
+
+	//c = getchar();
+	//while (c != EOF)
+	//{
+	//	if (c == ' ')
+	//	{
+	//		putchar('\n');
+	//	}
+	//	else
+	//	{
+	//		putchar(c);
+	//	}
+	//	c = getchar();
+	//}
+
+
+
+
+	//int c; // 1-13
+
+	//while ((c = getchar()) != EOF)
+	//{
+	//	if (c == ' ' || c == '\n' || c == '\t')
+	//	{
+	//		putchar('\n');
+	//	}
+	//	else
+	//	{
+	//		putchar('*');
+	//	}
+	//}
+	
+
+}
