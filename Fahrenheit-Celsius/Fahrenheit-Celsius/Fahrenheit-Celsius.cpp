985d53f7b70d59072523a3b5f37ddc81e01537d6
stoevd <stoevd@uni-sofia.bg>
2016-04-07 11:36:30 +0300

    Ex_1_full
    first chapter full :)

--- Fahrenheit-Celsius/Fahrenheit-Celsius/Fahrenheit-Celsius.cpp
+++ Fahrenheit-Celsius/Fahrenheit-Celsius/Fahrenheit-Celsius.cpp
@@ -1,12 +1,26 @@
 #include <stdio.h>
+#include <string.h>
 
 
-int main(void)
+//#define LOWER 0
+//#define UPPER 300
+//#define STEP 20
+// 1-15
+//void fahrtocelsius(void);
+//void celsiustofahr(void);
+
+
+//#define MAXLINE 1000
+// 1-19
+//int get_line(char s[], int lim);
+//void reverse(char s[]);
+
+
+int main()
 {
 
-	//printf("Hello World\n"); // 1-1
-	
 
+	//printf("Hello World\n"); // 1-1
 
 	//float fahr; // 1-4
 
@@ -18,7 +32,7 @@ int main(void)
 	//}
 
 
-
+	// 1-4
 
 	/*int celsius; // 1-4
 
@@ -28,7 +42,7 @@ int main(void)
 	}*/
 
 
-
+	// 1-6
 
 	/*int c; // 1-6
 	while ((c = getchar()) != eof) {
@@ -37,42 +51,151 @@ int main(void)
 	}
 	printf("\n%d\n", c != eof);
 */
-	
 
 
+//int c; // 1-12
 
-	//int c; // 1-12
+//c = getchar();
+//while (c != EOF)
+//{
+//	if (c == ' ')
+//	{
+//		putchar('\n');
+//	}
+//	else
+//	{
+//		putchar(c);
+//	}
+//	c = getchar();
+//}
+
+
+//int c; // 1-13
+
+//while ((c = getchar()) != EOF)
+//{
+//	if (c == ' ' || c == '\n' || c == '\t')
+//	{
+//		putchar('\n');
+//	}
+//	else
+//	{
+//		putchar('*');
+//	}
+//}
+
+	// 1-14
+	//char string[100]; // 1-14
+	//int c = 0;
+	//int count[100] = { 0 };
+
+	//while ((string[c] = getchar()) != EOF)
+	//{
+	//	if (string[c] >= ' ' && string[c] <= '~')
+	//	{
+	//		count[string[c] - '!']++;
+	//	}
+	//	c++;
+	//}
+
+	//for (c = 0; c < 93; c++)
+	//{
+	//	if (count[c] != 0) {			
+	//		for (int j = 1; j <= count[c]; j++)
+	//		{
+	//			printf("*");
+	//		}
+	//		printf("\n");
+	//	}
+	//}
+
+
+	//int c; // 1-15
+
+	//printf("Chose Conversion:\n");
+	//printf("Enter '1' : Fahrenheit to Celsius\n");
+	//printf("Enter '2' : Celsius to Fahrenheit\n");
+	//printf("- Enter your Choice:");
 
 	//c = getchar();
-	//while (c != EOF)
-	//{
-	//	if (c == ' ')
-	//	{
-	//		putchar('\n');
-	//	}
-	//	else
-	//	{
-	//		putchar(c);
-	//	}
-	//	c = getchar();
-	//}
 
 
+	//if (c == '1')
+	//	fahrtocelsius();
+	//else if (c == '2')
+	//	celsiustofahr();
+	//else
+	//	printf("Invalid Choice\n");
+
+// 1-19
+
+//	int len; // 1-19
+//	char line[MAXLINE];
+//
+//	while ((len = get_line(line, MAXLINE)) > 0)
+//	{
+//		reverse(line);
+//		printf("%s", line);
+//	}
+//
+//	return 0;
+//}
+
+// 1-15
+
+//void fahrtocelsius() //1-15
+//{
+//	float fahr;
+//
+//	for (fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+//		printf("%3.0f%6.1f\n", fahr, (5.0 / 9.0)*(fahr - 32.0));
+//}
+//
+//void celsiustofahr()
+//{
+//	float celsius;
+//
+//	for (celsius = LOWER; celsius <= UPPER; celsius = celsius + STEP)
+//		printf("%3.0f%6.1f\n", celsius, (9.0*celsius) / 5.0 + 32);
+//}
 
 
-	//int c; // 1-13
+// 1-19
 
-	//while ((c = getchar()) != EOF)
-	//{
-	//	if (c == ' ' || c == '\n' || c == '\t')
-	//	{
-	//		putchar('\n');
-	//	}
-	//	else
-	//	{
-	//		putchar('*');
-	//	}
-	//}
-	
-
-}
+//int get_line(char s[], int lim) // 1-19
+//{
+//	int i;
+//	int c;
+//
+//	for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; i++)
+//	{
+//		s[i] = c;
+//	}
+//	if (c == '\n')
+//	{
+//		s[i] = c;
+//		i++;
+//	}
+//	s[i] = '\0';
+//	return i;
+//}
+//
+//void reverse(char s[]) // 1-19
+//{
+//	int i = 0;
+//	int c;
+//	int j;
+//	int temp;
+//
+//	for (i = 0; s[i] != '\0'; i++)
+//	{}
+//	i--;
+//
+//	for (j = 0; j < i; j++)
+//	{
+//		temp = s[j];
+//		s[j] = s[i - 1];
+//		s[i - 1] = temp;
+//		i--;
+//	}
+//}
